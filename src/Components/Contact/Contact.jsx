9ee5858e763c841c6d3bc999a523a58a0346a625
Swordigo15 <annys.abubakar@gmail.com>
2024-03-13 11:29:12 +0700

    Update contact UI

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,19 +1,35 @@
 import React from 'react'
 import './Contact.css'
+import msg_icon from '../../assets/msg-icon.png'
+import mail_icon from '../../assets/mail-icon.png'
+import phone_icon from '../../assets/phone-icon.png'
+import location_icon from '../../assets/location-icon.png'
+import white_arrow from '../../assets/white-arrow.png'
 
 const Contact = () => {
   return (
     <div className='contact'>
         <div className="contact-col">
-            <h3>Send us a message</h3>
+            <h3>Send us a message <img src={msg_icon} alt="" /></h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est officia, rem pariatur asperiores nesciunt sit cupiditate odit vel accusamus culpa velit quaerat, illum quos fuga ad quibusdam quam minus architecto.</p>
             <ul>
-                <li>Contact@GreatStack.dev</li>
-                <li>+1 123-456-7890</li>
-                <li>77 Massachusetts Ave, Cambridge <br/> MA 02139, United States</li>
+                <li><img src={mail_icon} alt="" />Contact@GreatStack.dev</li>
+                <li><img src={phone_icon} alt="" />+1 123-456-7890</li>
+                <li><img src={location_icon} alt="" />77 Massachusetts Ave, Cambridge <br/> MA 02139, United States</li>
             </ul>
         </div>
-        <div className="contact-col"></div>
+        <div className="contact-col">
+            <form>
+                <label>Your Name</label>
+                <input type="text" name='name' placeholder='Enter your name' required/>
+                <label>Phone Number</label>
+                <input type="tel" name="phone" placeholder='Enter your mobile number' required/>
+                <label>Write Your Message Here</label>
+                <textarea name='message' rows='6' placeholder='Enter your message' required></textarea>
+                <button type="submit" className='btn dark-btn'>Submit Now <img src={white_arrow} alt="" /></button>
+            </form>
+            <span></span>
+        </div>
     </div>
   )
 }
