3f39c2d04b770a8aa6ab6937dabc37aca0f18d58
Swordigo15 <annys.abubakar@gmail.com>
2024-03-13 08:33:50 +0700

    update testimony

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -6,20 +6,39 @@ import user_1 from '../../assets/user-1.png'
 import user_2 from '../../assets/user-2.png'
 import user_3 from '../../assets/user-3.png'
 import user_4 from '../../assets/user-4.png'
+import { useRef } from 'react'
 
 const Testimonials = () => {
+
+    const slider = useRef();
+    let tx = 0;
+
+    const slideForward = () => {
+        if(tx > -50){
+            tx -= 25
+        }
+        slider.current.style.transform = `translateX(${tx}%)`;
+    }
+
+    const slideBackward = () => {
+        if(tx < 0){
+            tx += 25
+        }
+        slider.current.style.transform = `translateX(${tx}%)`;
+    }
+
   return (
     <div className='testimonials'>
-      <img src={next_icom} alt="" className='next-btn'/>
-      <img src={back_icom} alt="" className='back-btn'/>
+      <img src={next_icom} alt="" className='next-btn' onClick={slideForward}/>
+      <img src={back_icom} alt="" className='back-btn' onClick={slideBackward}/>
       <div className="slider">
-        <ul>
+        <ul ref={slider}>
             <li>
                 <div className="slide">
                     <div className="user-info">
                         <img src={user_1} alt="" />
                         <div>
-                            <h3>William Jackson</h3>
+                            <h3>William Jackson 1</h3>
                             <span>Edusity, USA</span>
                         </div>
                     </div>
@@ -31,7 +50,7 @@ const Testimonials = () => {
                     <div className="user-info">
                         <img src={user_2} alt="" />
                         <div>
-                            <h3>William Jackson</h3>
+                            <h3>William Jackson 2</h3>
                             <span>Edusity, USA</span>
                         </div>
                     </div>
@@ -43,7 +62,7 @@ const Testimonials = () => {
                     <div className="user-info">
                         <img src={user_3} alt="" />
                         <div>
-                            <h3>William Jackson</h3>
+                            <h3>William Jackson 3</h3>
                             <span>Edusity, USA</span>
                         </div>
                     </div>
@@ -55,7 +74,7 @@ const Testimonials = () => {
                     <div className="user-info">
                         <img src={user_4} alt="" />
                         <div>
-                            <h3>William Jackson</h3>
+                            <h3>William Jackson 4</h3>
                             <span>Edusity, USA</span>
                         </div>
                     </div>
