5954eaba9c2a4af34bb24e3cd8758030384a1568
Swordigo15 <annys.abubakar@gmail.com>
2024-03-13 08:38:14 +0700

    Add Contact

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Contact.css'
+
+const Contact = () => {
+  return (
+    <div className='contact'>
+        <div className="contact-col">
+            <h3>Send us a message</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est officia, rem pariatur asperiores nesciunt sit cupiditate odit vel accusamus culpa velit quaerat, illum quos fuga ad quibusdam quam minus architecto.</p>
+            <ul>
+                <li>Contact@GreatStack.dev</li>
+                <li>+1 123-456-7890</li>
+                <li>77 Massachusetts Ave, Cambridge <br/> MA 02139, United States</li>
+            </ul>
+        </div>
+        <div className="contact-col"></div>
+    </div>
+  )
+}
+
+export default Contact
